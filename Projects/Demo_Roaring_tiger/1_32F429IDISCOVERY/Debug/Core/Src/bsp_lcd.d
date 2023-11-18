d945f6b613d8d1477424107946d1d36b01ff49a3
Suleyman YALCINKAYA <yalcinkaya3434@gmail.com>
2023-11-18 23:26:57 +0300

    setup

---
+++ Projects/Demo_Roaring_tiger/1_32F429IDISCOVERY/Debug/Core/Src/bsp_lcd.d
@@ -0,0 +1,20 @@
+Core/Src/bsp_lcd.o: ../Core/Src/bsp_lcd.c ../Core/Inc/bsp_lcd.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Core/Inc/reg_util.h ../Core/Inc/ili9341_reg.h ../Core/Inc/board.h
+../Core/Inc/bsp_lcd.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Core/Inc/reg_util.h:
+../Core/Inc/ili9341_reg.h:
+../Core/Inc/board.h:
